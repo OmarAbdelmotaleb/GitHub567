3d9016d1f3d006af95baf4dcbf9f3df9c7ffe168
Omar Abdelmotaleb <oabdelmo@stevens.edu>
2023-10-11 19:40:32 -0400

    Removing one test

--- test_api.py
+++ test_api.py
@@ -7,11 +7,6 @@ class TestAPI(unittest.TestCase):
         # Test expected 200 status code
         self.assertEqual(getRepos("OmarAbdelmotaleb"), 200)
         self.assertEqual(getRepos("richkempinski"), 200)
-    
-    def testStatusFailed(self):
-        # Test that the API doesn't get back stuff for invalid names
-        self.assertNotEqual(getRepos("-"), 200)
-
 
 if __name__ == '__main__':
     unittest.main()
